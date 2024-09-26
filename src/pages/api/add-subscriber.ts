c5905b8593b86bb6a940ae28398c84ea7430710d
ppavlidis92 <116918115+ppavlidis92@users.noreply.github.com>
2024-09-26 19:33:16 +0300

    error in add-sub during deployment fix

--- src/pages/api/add-subscriber.ts
+++ src/pages/api/add-subscriber.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { addSubscriber } from '@/services/addSubscriberService';
+import axios from 'axios';
 
 export default async function handler(
   req: NextApiRequest,
@@ -11,20 +12,32 @@ export default async function handler(
     try {
       // Call the service to add a subscriber
       const response = await addSubscriber(name, email);
-      console.warn('Response Status from API:', response.status);
+      console.warn('Response Status from API:', response?.status);
+
       // Check if the response was successful
       if (response && response.status >= 200 && response.status < 300) {
         return res
           .status(200)
           .json({ message: 'Subscriber added successfully' });
+      } else {
+        return res.status(response?.status || 500).json({
+          message: response?.data?.Message || 'Failed to add subscriber',
+        });
       }
     } catch (error) {
-      // Check if there's an error message from the API
-      const errorMessage =
-        error.response?.data?.Message || 'Error adding subscriber';
-      return res
-        .status(error.response?.status || 500)
-        .json({ message: errorMessage });
+      // Narrow down the error type using axios' built-in type guards
+      if (axios.isAxiosError(error)) {
+        const errorMessage =
+          error.response?.data?.Message || 'Error adding subscriber';
+        return res.status(error.response?.status || 500).json({
+          message: errorMessage,
+        });
+      }
+
+      // Handle unexpected errors
+      return res.status(500).json({
+        message: 'An unexpected error occurred',
+      });
     }
   } else {
     return res.status(405).json({ message: 'Method not allowed' });
